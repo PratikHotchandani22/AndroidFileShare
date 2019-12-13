81dae79abb80069144d2504374e4fbefa3fab88f
PratikHotchandani22 <pratikhotchandani22@gmail.com>
2019-12-13 16:46:44 +0530

    Fourth Commit

--- FinalBluetoothShareApp2/app/src/main/java/pratik/application1/com/finalbluetoothshareapp/SendFile.java
+++ FinalBluetoothShareApp2/app/src/main/java/pratik/application1/com/finalbluetoothshareapp/SendFile.java
@@ -6,19 +6,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.DocumentsProvider;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.w3c.dom.DocumentType;
+
 import java.io.File;
+import java.util.ArrayList;
 
 public class SendFile extends AppCompatActivity {
 
-    Button selectFiles;
+    Button selectVideos;
     private String selectedFilePath;
 
     private static final int PICK_FILE_REQUEST = 1;
+
+    Button selectDocument;
+    Button selectImages;
+
+
+    String[] mimeTypes =
+            {"application/msword","application/vnd.openxmlformats-officedocument.wordprocessingml.document", // .doc & .docx
+                    "application/vnd.ms-powerpoint","application/vnd.openxmlformats-officedocument.presentationml.presentation", // .ppt & .pptx
+                    "application/vnd.ms-excel","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", // .xls & .xlsx
+                    "text/plain",
+                    "application/pdf",
+                    "application/zip"};
+
 
 
 
@@ -28,19 +48,65 @@ public class SendFile extends AppCompatActivity {
         setContentView(R.layout.activity_send_file);
 
 
-        Button selectFiles = (Button)findViewById(R.id.SelectFileBtn);
-        selectFiles.setOnClickListener(new View.OnClickListener() {
+        Button selectVideos = (Button)findViewById(R.id.SendVideosBtn);
+        final Button selectDocument  = (Button)findViewById(R.id.SendDocumentsBtn);
+        Button selectImages = (Button)findViewById(R.id.SendImagesBtn);
+
+
+        selectImages.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                //sets the select file to all types of files
-                intent.setType("*/*");
-                //allows to select data and return it
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                intent.setType("application/pdf");
+
+
+//                Intent intent = new Intent();
+//                //sets the select file to all types of files
+//                intent.setType("*/*");
+//
+//                //allows to select data and return it
+//                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
                 //starts new activity to select file and return data
                 startActivityForResult(Intent.createChooser(intent, "Choose File to Upload.."), PICK_FILE_REQUEST);
+            }
+        });
+
+
+        selectVideos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(Intent.createChooser(intent, "Choose File to Upload.."), PICK_FILE_REQUEST);
+
+
+            }
+        });
+
+        selectDocument.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+
+//
+           Intent intent = new Intent();
+////                //sets the select file to all types of files
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("application/pdf");
+                //String[] mimetypes = {"application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/msword"};
+                //intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
+////
+////                //allows to select data and return it
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+////
+////
+////
+////                //allows to select data and return it
+                startActivityForResult(Intent.createChooser(intent, "Choose File to Upload.."), PICK_FILE_REQUEST);
+
             }
         });
 
